1e57be2a12c3aa385b175ce0382541cfc47760e6
Peggy535 <peggy161152@gmail.com>
2022-02-06 18:26:05 +0000

    Changes to margin and padding of Hypno

--- src/routes/hypnotherapy.svelte
+++ src/routes/hypnotherapy.svelte
@@ -10,11 +10,11 @@
   </div>
   <div class="grid h-screen grid-cols-2 grid-rows-2 sm:grid-cols-4">
     <div class="grid col-start-1 col-end-3 row-start-1 row-end-2 sm:col-span-4 sm:row-span-1 items-start m-1 p-1">
-      <h2 class="text-4xl pr-2 sm:text-5xl sm:mb-20 md:text-6xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic 2xl:pr-8">
+      <h2 class="text-4xl pr-2 sm:text-5xl sm:mb-20 md:text-6xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
         A natural therapy that can help you to cope with the problems and challenges of everyday life like anxiety, phobias, fears, self esteem or even addiction to name but a few.
       </h2>
     </div>
-    <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-3 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 place-items-center 2xl:place-content-end">
+    <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-3 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 items-end sm:items-center 2xl:place-content-end">
       <p class="text-lg sm:text-xl md:text2xl leading-relaxed sm:leading-relaxed md:leading-relaxed lg:leading-loose"><span class="text-4xl md:text-6xl lg:text-6xl">Curative</span> hypnotherapy is a more 
         holistic feel to the sessions and work we do together. The word 'hypnotherapy' is derived from 'hypno' - relating to 'hypnosis', and 'therapy' because of the therapeutic suggestions that 
         are given during treatment.
@@ -22,12 +22,12 @@
     </div>
   </div>
   <div class="grid h-screen grid-cols-2 grid-rows-2 sm:grid-cols-4">
-    <div class="grid col-start-1 col-end-3 row-start-1 row-end-2 sm:col-span-4 sm:row-span-1 items-start lg:place-items-center">
-      <h2 class="text-3xl m-1 pr-2 sm:text-5xl sm:mb-20 md:text-6xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic 2xl:leading-snug 2xl:pr-8">
+    <div class="grid col-start-1 col-end-3 row-start-1 row-end-2 sm:col-span-4 sm:row-span-1 place-items-center">
+      <h2 class="text-3xl m-1 pr-2 sm:text-5xl sm:mb-20 md:text-6xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic 2xl:leading-snug md:pr-4 lg:pr-6 2xl:pr-8">
         Hypnosis is the profound state of relaxation, focused mind, and heightened sensory awareness achieved when applied techniques take the brain waves of a subject to the level of alpha or below. 
       </h2>
     </div>
-    <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-3 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 md:items-center 2xl:hidden">
+    <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-3 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 place-items-center 2xl:hidden">
       <p class="text-lg sm:text-xl leading-relaxed sm:leading-relaxed md:leading-relaxed lg:leading-loose"><span class="text-4xl md:text-6xl lg:text-7xl">You are</span> not asleep, but it is something 
         like a state just before you drift off to sleep - you can still hear everything, speak and even move around if you wished to. When you are in that state, your subconscious mind is more accessible 
         and open to suggestion.
@@ -50,7 +50,7 @@
       </p>
     </div>
     <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-4 sm:row-span-1 items-start">
-      <h2 class="text-5xl pr-2 sm:text-6xl sm:mb-20 md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic 2xl:pr-8">
+      <h2 class="text-5xl pr-2 sm:text-6xl sm:mb-20 md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
         If you think Hypnotherapy can help you or someone you know, just go ahead and contact me.
       </h2>
     </div>
