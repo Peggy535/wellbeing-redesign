7b1715c8803fc7d56fe7fb184ba4c3f721abdd00
Peggy535 <peggy161152@gmail.com>
2022-02-07 14:54:35 +0000

    Addition of last paragrapgh

--- src/routes/hypnotherapy.svelte
+++ src/routes/hypnotherapy.svelte
@@ -6,14 +6,14 @@
       <h1 class="font-Eiko-Thin font text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-12xl 2xl:text-14xl pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10">Hypnotherapy</h1>
     </div>
   </div>
-  <div class="grid grid-cols-1 grid-rows-1 lg:grid-cols-5 border-2">
+  <div class="grid grid-cols-1 grid-rows-1 lg:grid-cols-5">
     <div class="grid h-screen col-span-1 row-span-1 lg:col-start-2 lg:col-end-6 items-center m-1 p-1">
       <h2 class="text-4xl pr-2 sm:text-6xl sm:mb-20 md:text-7xl lg:text-7xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
         A natural therapy that can help you to cope with the problems and challenges of everyday life like anxiety, phobias, fears, self esteem or even addiction to name but a few.
       </h2>
     </div>
   </div>
-  <div class="grid grid-cols-1 grid-rows-1 sm:grid-cols-4 border-2">
+  <div class="grid grid-cols-1 grid-rows-1 sm:grid-cols-4">
     <div class="grid h-fit col-span-1 row-span-1 sm:col-span-3 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 items-end sm:items-center 2xl:place-content-end">
       <p class="text-lg sm:text-xl md:text2xl leading-relaxed sm:leading-relaxed md:leading-relaxed lg:leading-loose"><span class="text-5xl md:text-6xl lg:text-6xl">Curative</span> hypnotherapy is a more 
         holistic feel to the sessions and work we do together. The word 'hypnotherapy' is derived from 'hypno' - relating to 'hypnosis', and 'therapy' because of the therapeutic suggestions that 
@@ -21,14 +21,14 @@
       </p>
     </div>
   </div>
-  <div class="grid h-screen grid-cols-1 grid-rows-1 lg:grid-cols-5 border-2">
+  <div class="grid h-screen grid-cols-1 grid-rows-1 lg:grid-cols-5">
     <div class="grid col-span-1 row-span-1 lg:col-start-2 lg:col-end-6 place-items-center">
       <h2 class="text-4xl m-1 p-1 sm:text-6xl sm:mb-20 md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl text-right font-Eiko-Italic 2xl:leading-snug md:pr-4 lg:pr-6 2xl:pr-8">
         Hypnosis is the profound state of relaxation, focused mind, and heightened sensory awareness achieved when applied techniques take the brain waves of a subject to the level of alpha or below. 
       </h2>
     </div>
   </div>
-  <div class="grid grid-cols-1 grid-rows-1 sm:grid-cols-4 border-2">
+  <div class="grid grid-cols-1 grid-rows-1 sm:grid-cols-4">
     <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-3 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 place-items-center 2xl:hidden">
       <p class="text-lg sm:text-xl leading-relaxed sm:leading-relaxed md:leading-relaxed lg:leading-loose"><span class="text-5xl md:text-6xl lg:text-7xl">You are</span> not asleep, but it is something 
         like a state just before you drift off to sleep - you can still hear everything, speak and even move around if you wished to. When you are in that state, your subconscious mind is more accessible 
@@ -36,7 +36,7 @@
       </p>
     </div>
   </div>
-  <div class="h-0 invisible 2xl:visible 2xl:grid 2xl:h-auto 2xl:grid-cols-2 2xl:grid-rows-1 border-2">
+  <div class="h-0 invisible 2xl:visible 2xl:grid 2xl:h-auto 2xl:grid-cols-2 2xl:grid-rows-1">
     <div class="2xl:grid 2xl:col-start-1 2xl:col-end-2 2xl:row-start-1 2xl:row-end-2">
       <p class="2xl:text-xl 2xl:leading-loose m-2 p-2"><span class="text-4xl md:text-6xl lg:text-7xl">You are</span> not asleep, but it is something 
         like a state just before you drift off to sleep - you can still hear everything, speak and even move around if you wished to. When you are in that state, your subconscious mind is more accessible 
@@ -44,7 +44,7 @@
       </p>
     </div>
   </div>
-  <div class="grid h-screen grid-cols-2 grid-rows-2 sm:grid-cols-4 border-2">
+  <div class="grid h-screen grid-cols-2 grid-rows-2 sm:grid-cols-4">
     <div class="grid col-start-1 col-end-3 row-start-1 row-end-2 sm:col-span-3 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 place-items-center">
       <p class="text-lg sm:text-xl leading-relaxed sm:leading-relaxed md:leading-relaxed lg:leading-loose">It can be used to treat anxiety, phobias, compulsions (like over eating) and addictions (like smoking). 
         It can help with pain management and can ease the symptoms of chronic conditions such as asthma and IBS. Although it cannot cure physical illnesses or infections, it can help boost the immune system and 
@@ -52,9 +52,19 @@
       </p>
     </div>
     <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-4 sm:row-span-1 items-start">
-      <h2 class="text-4xl m-1 p-1 sm:text-6xl mb-20 md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic md:p-4 lg:p-6 2xl:p-8">
+      <h2 class="text-4xl m-1 p-1 sm:text-6xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic md:p-4 lg:p-6 2xl:p-8">
         If you think Hypnotherapy can help you or someone you know, just go ahead and contact me.
       </h2>
+    </div>
+  </div>
+  <div class="grid grid-cols-1 grid-rows-1 sm:grid-cols-4">
+    <div class="grid h-fit col-span-1 row-span-1 sm:col-span-3 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 items-end sm:items-center 2xl:place-content-end">
+      <h2 class="pb-10 font-Eiko-Italic text-4xl">A useful tip...</h2><br>
+      <p class="text-lg sm:text-xl md:text2xl leading-relaxed sm:leading-relaxed md:leading-relaxed lg:leading-loose"><span class="text-5xl md:text-6xl lg:text-6xl">Many</span>people 
+        imagine that the hypnotist has to do all the work without realising that the hypnotist only does all the work while they are talking and during the session. As the client drifts deeper into the 
+        experience they begin to discover that each session is a partnership of two minds. The hypnotists works the unconscious mind while the client follows instructions inside and outside the therapy 
+        session using the conscious mind, this allows the work to be completed both consciously and unconsciously. 
+      </p>
     </div>
   </div>
   <nav>
