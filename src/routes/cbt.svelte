93a0c6670d3255715cfbc1ab14ddb14a48354bc6
Peggy535 <peggy161152@gmail.com>
2022-02-14 19:25:50 +0000

    update to relaxation sketch

--- src/routes/cbt.svelte
+++ src/routes/cbt.svelte
@@ -1,4 +1,4 @@
-<section class="absolute z-20 top-0 w-screen overflow-x-hidden text-lime-3">
+<section class="absolute z-20 top-0 w-screen overflow-x-hidden text-rose-main">
   <div class="grid h-screen w-screen grid-rows-4 grid-cols-1">
     <div class="grid row-start-3 row-end-4 items-end">
       <h1 class="font-Eiko-Thin font text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-12xl 2xl:text-14xl pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10">Mindful CBT</h1>
@@ -6,8 +6,8 @@
     </div>
   </div>
   <div class="grid h-screen grid-cols-1 sm:grid-cols-4">
-    <div class="grid h-screen col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-3 row-span-5 items-center m-1 p-1">
-      <h2 class="text-4xl pr-2 sm:leading-snug mb-20 md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-6xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
+    <div class="grid h-screen col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-2 items-center m-1 p-1">
+      <h2 class="text-4xl pr-2 sm:leading-snug mb-20 md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-6xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
         Mindful CBT is a talk therapy that focuses on the particular ways in which an individual thinks, behaves and acts, in the attempt to deal successfully with their behavioural, 
         emotional and social problems and issues.
       </h2>
@@ -15,7 +15,7 @@
   </div>
   <div class="grid grid-cols-1 sm:grid-cols-4">
     <div class="grid h-fit col-span-1 sm:col-span-2 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 items-center sm:items-center 2xl:place-content-end">
-      <p class="text-sm text-slate-600 leading-relaxed"><span class="text-lime-3 text-5xl font-Eiko-Italic md:text-6xl lg:text-6xl">The</span> difference 
+      <p class="text-sm text-slate-600 leading-relaxed"><span class="text-rose-2 text-5xl font-Eiko-Italic md:text-6xl lg:text-6xl">The</span> difference 
         between psychotherapy and behavioural therapy is that psychotherapy tends to emphasise the importance of the personal meaning placed upon things and how the majority 
         of thinking patterns begin in childhood. Behavioural therapy relates to everything the person does. How they act with other people, when they are alone. How they avoid certain aspects 
         of life and how they engage and interact with the world around them. How active or inactive towards being involved in participating in life on a healthy level.
@@ -24,7 +24,7 @@
   </div>
   <div class="grid grid-cols-1 sm:grid-cols-4">
     <div class="grid h-fit col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-2 lg:col-end-6 items-end m-1 p-1">
-      <h2 class="text-4xl pr-2 sm:text-6xl md:text-7xl lg:text-7xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
+      <h2 class="text-4xl pr-2 md:pr-4 lg:pr-6 2xl:pr-8 my-20 sm:text-6xl md:text-7xl lg:text-7xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic">
         My clients' have experienced amazing results. Especially in stress related disorders.
       </h2>
     </div>
