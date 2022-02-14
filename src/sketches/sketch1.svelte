73a6bcf4fc4c226d282b722cb6fcc9390e4bb39e
Peggy535 <peggy161152@gmail.com>
2022-02-14 16:51:19 +0000

    update to relaxation sketch

--- src/sketches/sketch1.svelte
+++ src/sketches/sketch1.svelte
@@ -2,8 +2,8 @@
 import P5 from "p5-svelte";
 
 const sketch = (p5) => {
-  let w = p5.windowWidth;
-  let h = p5.windowHeight;
+  let w = p5.displayWidth;
+  let h = p5.displayHeight;
   p5.setup = () => {
     p5.createCanvas(w, h);
     //p5.background(184,110,122);
