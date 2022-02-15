b5b5da289bb1d2c7532ea734c4dec64118d76df6
Peggy535 <peggy161152@gmail.com>
2022-02-15 18:24:18 +0000

    Changes to Hypnotherapy page

--- src/routes/relaxation.svelte
+++ src/routes/relaxation.svelte
@@ -10,12 +10,12 @@
 <section class="absolute z-20 top-0 w-screen overflow-x-hidden overscroll-none text-beige-3">
   <div class="grid h-screen w-screen grid-rows-4 grid-cols-1">
     <div class="grid row-start-3 row-end-4 items-end">
-      <h1 class="font-Eiko-Thin text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-10xl pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10">Relaxation Therapy</h1>
+      <h1 class="font-Eiko-Thin text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-9xl pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10">Relaxation Therapy</h1>
     </div>
   </div>
   <div class="grid h-screen grid-cols-1 sm:grid-cols-4  grid-rows-6">
     <div class="grid col-span-1 row-span-1 sm:col-span-full">
-      <p class="font-Eiko-Thin text-2xl m-1 p-1 text-slate-800">Hypnotherapy not for you? I've got you covered.</p>
+      <p class="font-Eiko-Thin text-md pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10 text-stone-700">Hypnotherapy not for you? I've got you covered.</p>
     </div>
     <div class="grid col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-2 row-span-5 items-start xl:items-center m-1 p-1">
       <h2 class="text-4xl pr-2 sm:text-4xl sm:leading-snug mb-20 md:text-5xl lg:text-6xl xl:text-6xl 2xl:text-6xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
@@ -26,33 +26,31 @@
   </div>
   <div class="grid h-screen grid-cols-1 sm:grid-cols-4 grid-rows-6 items-center">
     <div class="grid col-span-1 sm:col-start-1 sm:col-end-4 md:col-end-3 2xl:col-end-3 row-span-2">
-      <p class="text-sm lg:text-lg m-1 p-1 text-slate-800"><span class="font-Eiko-Italic text-4xl text-beige-3">Relaxation</span> therapy has been 
+      <p class="text-sm lg:text-lg m-1 p-1 text-stone-700"><span class="font-Eiko-Italic text-4xl text-beige-3">Relaxation</span> therapy has been 
         specifically designed for Anxiety and Stress relief. It helps reduce muscle tension and stress, lowers blood pressure, and controls pain.
       </p>
     </div>
     <div class="grid col-span-1 sm:col-start-1 sm:col-end-4 md:col-end-3 2xl:col-end-3 row-span-2">
-      <p class="text-sm lg:text-lg m-1 p-1 text-slate-800"><span class="font-Eiko-Italic text-4xl text-beige-3">What is deep relaxation therapy?</span>
+      <span class="font-Eiko-Italic text-4xl text-beige-3 m-1 p-1">What is deep relaxation therapy?</span>
+      <p class="text-sm lg:text-lg m-1 p-1 text-stone-700">
         The relaxation response is a technique designed to elicit a state of deep relaxation in which breathing, pulse rate, blood pressure and metabolism are decreased. 
         Training our bodies on a daily basis to achieve this state of relaxation can lead to enhanced mood, lowered blood pressure and reduction of lifestyle stress.
       </p>
     </div>
-    <div class="grid col-span-1 sm:col-start-1 sm:col-end-4 md:col-end-3 2xl:col-end-3 row-span-2 m-1 p-1 text-slate-800">
+    <div class="grid col-span-1 sm:col-start-1 sm:col-end-4 md:col-end-3 2xl:col-end-3 row-span-2 m-1 p-1 text-stone-700">
       <ul class="grid">
         <li class="mb-5 md:mb-0 text-sm lg:text-lg">Techniques used are visual imagery and body awareness to move a person into a deep state of relaxation, involving:</li>
-        <li class="m-1 p-1 font-Eiko-Italic text-2xl">Breathwork Techniques</li>
-        <li class="m-1 p-1 font-Eiko-Italic text-2xl">Progressive muscle relaxation</li>
-        <li class="m-1 p-1 font-Eiko-Italic text-2xl">Meditation</li>
-        <li class="m-1 p-1 font-Eiko-Italic text-2xl">Guided imagery</li>
+        <li class="m-1 p-1 font-Eiko-Italic text-xl text-beige-3 text-center">Breathwork Techniques</li>
+        <li class="m-1 p-1 font-Eiko-Italic text-xl text-beige-3 text-center">Progressive muscle relaxation</li>
+        <li class="m-1 p-1 font-Eiko-Italic text-xl text-beige-3 text-center">Meditation</li>
+        <li class="m-1 p-1 font-Eiko-Italic text-xl text-beige-3 text-center">Guided imagery</li>
       </ul>
     </div>
   </div>
-  <div class="grid grid-cols-1 grid-rows-3 sm:grid-cols-4 h-screen items-center">
+  <div class="grid grid-cols-1 grid-rows-3 sm:grid-cols-4 h-fit items-center my-10">
     <h2 class="grid col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-3 row-span-2 text-right text-5xl xl:text-6xl m-1 p-1 pr-2 md:pr-4 font-Eiko-Italic">
-      If you think Relaxation therapy can help you or someone you know, just go ahead and contact me.
+      To book your online therapy click here...
     </h2>
-    <p class="grid col-span-1 sm:col-start-2 sm:col-end-5 row-span-1 m-1 p-1 pr-2 md:pr-4 text-right text-3xl font-Eiko-Italic">
-      £35 for a 30 min or £45 for 40min online session's.
-    </p>
   </div>
   <nav>
     <a href="/">Home | </a>
