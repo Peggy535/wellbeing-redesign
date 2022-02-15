2aeb75c83f09c3588e5112053afd92dce9d82245
Peggy535 <peggy161152@gmail.com>
2022-02-15 18:36:13 +0000

    Changes to relaxation and cbt pages

--- src/routes/cbt.svelte
+++ src/routes/cbt.svelte
@@ -1,13 +1,13 @@
 <section class="absolute z-20 top-0 w-screen overflow-x-hidden text-rose-main">
   <div class="grid h-screen w-screen grid-rows-4 grid-cols-1">
     <div class="grid row-start-3 row-end-4 items-end">
-      <h1 class="font-Eiko-Thin font text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-12xl 2xl:text-14xl pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10">Mindful CBT</h1>
-      <cite class="text-slate-600 text-sm pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10">(Cognitive Behavioural Therapy)</cite>
+      <h1 class="font-Eiko-Thin text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-12xl 2xl:text-14xl pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10">Mindful CBT</h1>
+      <p class="text-stone-700 text-sm pl-3 sm:pl-3 md:pl-4 lg:pl-6 2xl:pl-10">(Cognitive Behavioural Therapy)</p>
     </div>
   </div>
   <div class="grid h-screen grid-cols-1 sm:grid-cols-4">
-    <div class="grid h-screen col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-2 items-center m-1 p-1">
-      <h2 class="text-4xl pr-2 sm:leading-snug mb-20 md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-6xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
+    <div class="grid col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-2 items-center m-1 p-1">
+      <h2 class="text-5xl pr-2 sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-7xl text-right font-Eiko-Italic md:pr-4 lg:pr-6 2xl:pr-8">
         Mindful CBT is a talk therapy that focuses on the particular ways in which an individual thinks, behaves and acts, in the attempt to deal successfully with their behavioural, 
         emotional and social problems and issues.
       </h2>
@@ -15,7 +15,7 @@
   </div>
   <div class="grid grid-cols-1 sm:grid-cols-4">
     <div class="grid h-fit col-span-1 sm:col-span-2 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 items-center sm:items-center 2xl:place-content-end">
-      <p class="text-sm text-slate-600 leading-relaxed"><span class="text-rose-2 text-5xl font-Eiko-Italic md:text-6xl lg:text-6xl">The</span> difference 
+      <p class="text-sm text-stone-700 leading-relaxed lg:text-xl"><span class="text-rose-main text-6xl font-Eiko-Italic md:text-6xl lg:text-6xl">The</span> difference 
         between psychotherapy and behavioural therapy is that psychotherapy tends to emphasise the importance of the personal meaning placed upon things and how the majority 
         of thinking patterns begin in childhood. Behavioural therapy relates to everything the person does. How they act with other people, when they are alone. How they avoid certain aspects 
         of life and how they engage and interact with the world around them. How active or inactive towards being involved in participating in life on a healthy level.
@@ -24,25 +24,22 @@
   </div>
   <div class="grid grid-cols-1 sm:grid-cols-4">
     <div class="grid h-fit col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-2 lg:col-end-6 items-end m-1 p-1">
-      <h2 class="text-4xl pr-2 md:pr-4 lg:pr-6 2xl:pr-8 my-20 sm:text-6xl md:text-7xl lg:text-7xl xl:text-7xl 2xl:text-8xl text-right font-Eiko-Italic">
+      <h2 class="text-4xl pr-2 md:pr-4 lg:pr-6 2xl:pr-8 my-20 sm:text-6xl md:text-7xl lg:text-8xl xl:text-8xl 2xl:text-8xl text-right font-Eiko-Italic">
         My clients' have experienced amazing results. Especially in stress related disorders.
       </h2>
     </div>
   </div>
   <div class="grid grid-cols-1 sm:grid-cols-4">
     <div class="grid h-fit col-span-1 sm:col-span-2 m-1 p-1 lg:m-2 lg:p-2 items-end sm:items-center 2xl:place-content-end">
-      <p class="text-sm text-slate-600">The advantage that CBT has over other therapies is that clients undergoing CBT treatment are actually learning the educational aspects of the therapy, which is then empowering them to become 
+      <p class="text-sm text-stone-700 lg:text-xl">The advantage that CBT has over other therapies is that clients undergoing CBT treatment are actually learning the educational aspects of the therapy, which is then empowering them to become 
         their own personal therapist and to be more aware of the circumstances, situations, behaviours and people that could cause them to lapse and therefore to be able to avoid these aspects in their lives.
       </p>
     </div>
   </div>
-  <div class="grid grid-cols-1 grid-rows-3 sm:grid-cols-4 h-screen items-center">
+  <div class="grid h-fit grid-cols-1 grid-rows-3 sm:grid-cols-4 items-center my-10">
     <h2 class="grid col-span-1 sm:col-start-2 sm:col-end-5 lg:col-start-3 row-span-2 text-right text-5xl xl:text-6xl m-1 p-1 pr-2 md:pr-4 font-Eiko-Italic">
-      If you think CBT can help you or someone you know, just go ahead and contact me.
+      To book your online therapy click here...
     </h2>
-    <p class="grid col-span-1 sm:col-start-2 sm:col-end-5 row-span-1 m-1 p-1 pr-2 md:pr-4 text-right text-3xl font-Eiko-Italic">
-      £55 for a 45 min online session.
-    </p>
   </div>
   <nav>
     <a href="/">Home | </a>
