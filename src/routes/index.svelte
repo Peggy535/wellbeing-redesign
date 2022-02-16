f948162c35b32a6b05f0e30547de4f5bf2363018
Peggy535 <peggy161152@gmail.com>
2022-02-16 17:01:22 +0000

    changes to all index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,12 +18,12 @@ import Sketch4 from '../sketches/sketch4.svelte';
     <div class="grid col-start-1 col-end-3 row-start-1 row-end-2 sm:col-span-4 sm:row-span-1 xl:col-span-5 items-start">
       <h1 class="text-7xl pr-2 md:pr-10 lg:pr-5 xl:pr-5 sm:text-9xl md:text-10xl lg:text-13xl xl:text-14xl 2xl:text-15xl text-right font-Eiko-Italic">Welcome.</h1>
       <div class="flex flex-col text-stone-900 pr-2">
-          <p class="text-lg m-1 p-1 pr-2 md:pr-10 text-right font-Eiko-Italic text-zinc-800">Your go to specialist in anxiety and stress relief:<br>Curative Hypnotherapy<br>Relaxation Therapy<br>CBT/MCBT
+          <p class="text-lg m-1 p-1 pr-2 md:pr-10 text-right font-Eiko-Italic text-rose-50">Your go to specialist in anxiety and stress relief:<br>Curative Hypnotherapy<br>Relaxation Therapy<br>CBT/MCBT
           </p>
         </div>
     </div>
     <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-2 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 place-items-start">
-      <p class="text-lg leading-relaxed lg:text-xl lg:leading-loose text-zinc-800"><span class="font-Eiko-Italic text-rose-4 text-7xl lg:text-9xl">Hello</span> and welcome to my website, I hope you find both it and the words helpful, healing and inspiring. Whatever you are facing at this time 
+      <p class="text-lg leading-relaxed lg:text-xl lg:leading-loose text-rose-50"><span class="font-Eiko-Italic text-rose-4 text-7xl lg:text-9xl">Hello</span> and welcome to my website, I hope you find both it and the words helpful, healing and inspiring. Whatever you are facing at this time 
         in your life, no matter what it is, please be assured there is a reason and that there is help for you if you wish to receive it.</p>
     </div>
   </div>
@@ -33,10 +33,10 @@ import Sketch4 from '../sketches/sketch4.svelte';
       You are beautiful, unique and deserving of all good things.<cite class="text-sm">- Justine Hodgson</cite>
     </blockquote>
     <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-2 sm:row-span-1 xl:col-span-2 place-content-evenly lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2">
-      <p class=" text-lg leading-relaxed lg:text-xl text-zinc-800 lg:leading-loose"><span class="font-Eiko-Italic text-rose-4 text-8xl lg:text-9xl">By</span> now hopefully, you've pressed the play button on my welcome message above. 
+      <p class=" text-lg leading-relaxed lg:text-xl text-rose-50 lg:leading-loose"><span class="font-Eiko-Italic text-rose-4 text-8xl lg:text-9xl">By</span> now hopefully, you've pressed the play button on my welcome message above. 
         As a hypnotherapist, it's important you get a feel for my voice and personality if we are going to do some work together. 
         I'm also a strong believer in being as real on paper as I am in person, hopefully this all resonates and feels authentic for every visitor to my site.</p><br/>
-      <p class=" text-lg leading-relaxed lg:text-xl text-zinc-800 lg:leading-loose">Just as so many before me and many to follow, I've had my own path to travel and it's not always been straight 
+      <p class=" text-lg leading-relaxed lg:text-xl text-rose-50 lg:leading-loose">Just as so many before me and many to follow, I've had my own path to travel and it's not always been straight 
         forward but it has led me to where I am today and it continues to get better and convince me that, miracles actually do happen.</p>
     </div>
   </div>
@@ -49,7 +49,7 @@ import Sketch4 from '../sketches/sketch4.svelte';
   </div>
   <div class="grid h-fit grid-cols-1 sm:grid-cols-4 xl:grid-cols-5">
     <div class="grid col-start-1 col-end-3 row-start-2 row-end-3 sm:col-span-2 sm:row-span-1 lg:col-span-2 m-1 p-1 lg:m-2 lg:p-2 items-start">
-      <p class=" text-lg leading-relaxed lg:text-xl text-zinc-800 lg:leading-loose"><span class="font-Eiko-Italic text-rose-4 text-7xl lg:text-9xl">My</span>desire today and every day hereon, is to make people feel less broken, so they 
+      <p class=" text-lg leading-relaxed lg:text-xl text-rose-50 lg:leading-loose"><span class="font-Eiko-Italic text-rose-4 text-7xl lg:text-9xl">My</span>desire today and every day hereon, is to make people feel less broken, so they 
         can experience more joy and happiness and less stress in their lives. This in turn can only benefit those around them, and there we have it, a beautiful ripple effect begins and reaches far and 
         wide, as far as the imagination can take you!
       </p>
